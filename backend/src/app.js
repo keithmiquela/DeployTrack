c7ea5e85fa90f99bd8e55bb5bc4e9ea1e84278c7
KeithVM <keithmiquela@gmail.com>
2025-08-19 22:50:55 -0400

    feat: set up for deployment

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,14 +1,20 @@
-require('dotenv').config()
+import path from 'path';
+import express from 'express';
+import mongoose from 'mongoose';
+import cors from 'cors';
+import dotenv from 'dotenv';
 
-const express = require('express')
-const mongoose = require('mongoose')
-const cors = require('cors')
+import deploymentRoutes from './routes/deployments.js';
+import userRoutes from './routes/users.js';
 
-const deploymentRoutes = require('./routes/deployments')
-const userRoutes = require('./routes/users')
+dotenv.config();
 
 // express app
 const app = express()
+
+import { fileURLToPath } from 'url';
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 // middleware
 app.use(cors());
@@ -23,11 +29,19 @@ app.use((req, res, next) => {
 app.use('/deployments/',deploymentRoutes)
 app.use('/user/', userRoutes)
 
+if(process.env.NODE_ENV === "production"){
+  app.use(express.static(path.join(__dirname, "../../frontend/dist")));
+
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "../../frontend/dist/index.html"))
+  })
+}
+
 //connect to db
 mongoose.connect(process.env.MONGO_URI)
   .then(() => {
     app.listen(process.env.PORT, () => {
-      console.log(`Connected to db, listening on port ${process.env.PORT}`)
+      console.log(`Connected to db, listening on port ${process.env.PORT}`);
     })
   })
   .catch((error) => {
